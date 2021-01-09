da73c2c67afda12598cd9d6deb6e3e61ee75c487
mariano.sanfilippo <mariano.sanfilippo@uala.com.ar>
2021-01-09 16:06:16 -0300

    fix in variable names

--- accounts/main.tf
+++ accounts/main.tf
@@ -1,5 +1,5 @@
 module "account" {
   source = "git@github.com:mnsanfilippo/terraform-modules.git"
-  name = var.account_name
-  email = var.account_email
+  account_name = var.account_name
+  account_email = var.account_email
 }
