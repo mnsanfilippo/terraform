64dcc22a763a2c73c1cc9cdff26b78a5ce062869
mariano.sanfilippo <mariano.sanfilippo@uala.com.ar>
2021-01-09 14:25:33 -0300

    moved test to test folder

--- accounts/dev/test/terraform_aws_organizations_account_test.go
+++ accounts/dev/test/terraform_aws_organizations_account_test.go
@@ -14,7 +14,7 @@ import (
 	"testing"
 )
 var terraformPlan = true
-var repositoriesPath = "github-repositories"
+
 
 func TestOrganizationAccountCreation(t *testing.T) {
 
@@ -27,7 +27,7 @@ func TestOrganizationAccountCreation(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 
 		// The path to where our Terraform code is located
-		TerraformDir: ".",
+		TerraformDir: "../",
 
 		//Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
