96df32383b6ea0abc835da3ac48cb87b5c0e3467
mariano.sanfilippo <mariano.sanfilippo@uala.com.ar>
2021-01-09 16:11:23 -0300

    fix in source path

--- accounts/main.tf
+++ accounts/main.tf
@@ -1,5 +1,5 @@
 module "account" {
-  source = "git@github.com:mnsanfilippo/terraform-modules.git"
+  source = "git@github.com:mnsanfilippo/terraform-modules/organization/accounts.git"
   account_name = var.account_name
   account_email = var.account_email
 }
